3fdbebf162d45ea1e682af05d25d7f79cd580146
printfKrzysztof <krzysztofbortnowski@outlook.com>
2023-04-16 17:45:25 +0200

    Ćwiczenia w domu

--- Ćwiczenia/script.js
+++ Ćwiczenia/script.js
@@ -1,7 +1,169 @@
 // JavaScript
 var liczba = 1
-function myFunction() {
+
+const DBOpenRequest = window.indexedDB.open('IndexedDB', 10);
+let db //Info about database
+DBOpenRequest.onerror = (event) => {
+  console.log("Error opening database");
+};
+
+DBOpenRequest.onsuccess = (event) => {
+  console.log("Database opening succeed");
+
+  db = event.target.result;
+
+};
+
+
+DBOpenRequest.onupgradeneeded = (event) => {
+  db = event.target.result;
+
+  db.onerror = (event) => {
+    console.log('Error loading database.');
+  };
+
+  // Create an objectStore for this database
+  const objectStore = db.createObjectStore('IndexedDB', { keyPath: 'id' });
+
+  // Define what data items the objectStore will contain
+  objectStore.createIndex('firstname', 'firstname', { unique: false });
+  objectStore.createIndex('lastname', 'lastname', { unique: false });
+  objectStore.createIndex('email', 'email', { unique: false });
+  objectStore.createIndex('zip', 'zip', { unique: false });
+  objectStore.createIndex('nip', 'nip', { unique: false });
+  objectStore.createIndex('phone', 'phone', { unique: false });
+
+  console.log("Created object store.");
+};
+
+function saveData(e) {
+  // Prevent default, as we don't want the form to submit in the conventional way
+  //e.preventDefault();
+
+  var table = document.getElementById("clients_data_table");
+  for (var i = 1, row; row = table.rows[i]; i++) {
+    //iterate through rows
+    //rows would be accessed using the "row" variable assigned in the for loop
+    var vfirstname = row.cells[0].innerHTML;
+    var vlastname = row.cells[1].innerHTML;
+    var vemail = row.cells[2].innerHTML;
+    var vzip = row.cells[3].innerHTML;
+    var vnip = row.cells[4].innerHTML;
+    var vphone = row.cells[5].innerHTML;
+
+    
+
+    // Grab the values entered into the form fields and store them in an object ready for being inserted into the IndexedDB
+    const newItem = [
+      { id: i, firstname: vfirstname, lastname: vlastname, email: vemail, zip: vzip, nip: vnip, phone: vphone },
+    ];
+    
+    // Open a read/write DB transaction, ready for adding the data
+    const transaction = db.transaction(['IndexedDB'], 'readwrite');
+
+    // Report on the success of the transaction completing, when everything is done
+    transaction.oncomplete = () => {
+      console.log('Transaction completed: database modification finished.');
+
+      // Update the display of data to show the newly added item, by running displayData() again.
+    };
+
+    // Handler for any unexpected error
+    transaction.onerror = () => {
+      console.log(`Transaction not opened due to error: ${transaction.error}`);
+    };
+
+    // Call an object store that's already been added to the database
+    const objectStore = transaction.objectStore('IndexedDB');
+    console.log(objectStore.indexNames);
+    console.log(objectStore.keyPath);
+    console.log(objectStore.name);
+    console.log(objectStore.transaction);
+    console.log(objectStore.autoIncrement);
+
+    // Make a request to add our newItem object to the object store
+    const objectStoreRequest = objectStore.add(newItem[0]);
+    objectStoreRequest.onsuccess = (event) => {
+
+      console.log('Request successful.');
+
+    };
+  }
+};
+
+//Loading data
+function loadData() {
+  // First clear the content of the task list so that you don't get a huge long list of duplicate stuff each time
+  // the display is updated.
+  //while (taskList.firstChild) {
+  // taskList.removeChild(taskList.lastChild);
+  //  }
+  var table = document.getElementById("clients_data_table");
+  while(table.rows.length > 0) {
+    table.deleteRow(0);
+  }
+  // Open our object store and then get a cursor list of all the different data items in the IDB to iterate through
+  const objectStore = db.transaction('IndexedDB').objectStore('IndexedDB');
+  objectStore.openCursor().onsuccess = (event) => {
+    const cursor = event.target.result;
+    // Check if there are no (more) cursor items to iterate through
+    if (!cursor) {
+      // No more items to iterate through, we quit.
+      console.log('Entries all displayed.');
+      return;
+    }
+
+    // Read the shit and add it to table
+    const { firstname, lastname, email, zip, nip, phone } = cursor.value;
+    var t = document.getElementById('clients_data_table');
+    var r = t.insertRow(-1);
+    var c = r.insertCell(0);
+    c.innerHTML = firstname;
+    c = r.insertCell(1);
+    c.innerHTML = lastname;
+    c = r.insertCell(2);
+    c.innerHTML = email;
+    c = r.insertCell(3);
+    c.innerHTML = zip;
+    c = r.insertCell(4);
+    c.innerHTML = nip;
+    c = r.insertCell(5);
+    c.innerHTML = phone;
+
+    // continue on to the next item in the cursor
+
+    console.log('Printed row');
+    cursor.continue();
+  };
+};
+
+
+
+// 2 Functions that are used in Dodaj button
+function toTable() {
+  var t = document.getElementById('clients_data_table');
+  var r = t.insertRow(-1);
+  var c = r.insertCell(0);
+  c.innerHTML = document.getElementById("firstname").value;
+  c = r.insertCell(1);
+  c.innerHTML = document.getElementById("lastname").value;
+  c = r.insertCell(2);
+  c.innerHTML = document.getElementById("email").value;
+  c = r.insertCell(3);
+  c.innerHTML = document.getElementById("zip").value;
+  c = r.insertCell(4);
+  c.innerHTML = document.getElementById("nip").value;
+  c = r.insertCell(5);
+  c.innerHTML = document.getElementById("phone").value;
+
+}
+
+function generateDataAndAppend() {
   if (liczba == 1) {
+    document.getElementById("firstname").value = "kacper";
+    document.getElementById("lastname").value = "Banek";
+    document.getElementById("zip").value = "90-000";
+    document.getElementById("nip").value = "022-41-11-111";
     document.getElementById("ipv4").value = "10.1.1.200";
     document.getElementById("webpage").value = "https://www.jacek.pl";
     document.getElementById("email").value = "jb@gmail.com";
@@ -11,6 +173,10 @@ function myFunction() {
     document.getElementById("phone").value = "123456789";
   }
   else if (liczba == 2) {
+    document.getElementById("firstname").value = "Adam";
+    document.getElementById("lastname").value = "Kuzin";
+    document.getElementById("zip").value = "90-120";
+    document.getElementById("nip").value = "022-41-12-111";
     document.getElementById("ipv4").value = "10.1.2.200";
     document.getElementById("webpage").value = "https://www.jacekgacek.pl";
     document.getElementById("email").value = "jb1@gmail.com";
@@ -21,6 +187,10 @@ function myFunction() {
 
   }
   else if (liczba == 3) {
+    document.getElementById("firstname").value = "kacperw";
+    document.getElementById("lastname").value = "Barnek";
+    document.getElementById("zip").value = "60-000";
+    document.getElementById("nip").value = "022-21-11-111";
     document.getElementById("ipv4").value = "10.1.12.200";
     document.getElementById("webpage").value = "https://www.jg.pl";
     document.getElementById("email").value = "jb2@gmail.com";
@@ -30,6 +200,10 @@ function myFunction() {
     document.getElementById("phone").value = "123456319";
   }
   else if (liczba == 4) {
+    document.getElementById("firstname").value = "kacpera";
+    document.getElementById("lastname").value = "Banekei";
+    document.getElementById("zip").value = "70-000";
+    document.getElementById("nip").value = "021-41-11-111";
     document.getElementById("ipv4").value = "100.1.1.200";
     document.getElementById("webpage").value = "https://www.pol.pl";
     document.getElementById("email").value = "jb3@gmail.com";
@@ -39,6 +213,10 @@ function myFunction() {
     document.getElementById("phone").value = "123356789";
   }
   else if (liczba == 5) {
+    document.getElementById("firstname").value = "kacpers";
+    document.getElementById("lastname").value = "Banekson";
+    document.getElementById("zip").value = "80-000";
+    document.getElementById("nip").value = "022-41-11-211";
     document.getElementById("ipv4").value = "10.123.1.200";
     document.getElementById("webpage").value = "https://www.mina.pl";
     document.getElementById("email").value = "jb4@gmail.com";
@@ -49,4 +227,6 @@ function myFunction() {
     liczba = 0;
   }
   liczba = liczba + 1;
+  toTable();
 }
+
