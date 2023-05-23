d81bee0256b3cc596715445aa77bf0c941c8c9e8
printfKrzysztof <krzysztofbortnowski@outlook.com>
2023-05-23 15:51:33 +0200

    dodanie flagi w celu kilku przeszkuan

--- lab8/script.js
+++ lab8/script.js
@@ -297,22 +297,26 @@ function clearDBStore(storename) {
 }
 
 function searchData() {
-  var input, filter, table, tr, td, i, txtValue;
+  var input, filter, table, tr, td, i, txtValue, flag;
   input = document.getElementById("search");
   filter = input.value.toUpperCase();
   table = document.getElementById("clients_data_table");
   tr = table.getElementsByTagName("tr");
-  for (i = 0; i < tr.length; i++) {
+  for (i = 1; i < tr.length+1; i++) {
+    flag = false
     for (j = 0; j < 6; j++) {
       td = tr[i].getElementsByTagName("td")[j];
       if (td) {
         txtValue = td.textContent || td.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
-        }
+          flag=true
+        } 
       }
+    }
+    if (flag == true) {
+      tr[i].style.display = "";
+    } else {
+      tr[i].style.display = "none";
     }
   }
 }
